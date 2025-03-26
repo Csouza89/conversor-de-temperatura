d5e19cdc2c0d77487f818df6c63a0b2e17dadcc6
Cassio Souza <160286487+Csouza89@users.noreply.github.com>
2025-03-26 20:04:29 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,75 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <div class="container">
+      <h1 class="titulo">Conversor de Temperatura</h1>
+      
+      <div class="input-container">
+        <label>Celsius (°C): </label>
+        <input type="number" [(ngModel)]="celsius" (input)="conversorDeCelsius()">
+      </div>
+      
+      <div class="input-container">
+        <label>Fahrenheit (°F): </label>
+        <input type="number" [(ngModel)]="fahrenheit" (input)="conversorFahrenheit()">
+      </div>
 
-    
+      <div class="input-container">
+        <label>Kelvin (K): </label>
+        <input type="number" [(ngModel)]="kelvin" (input)="conversorKelvin()">
+      </div>
+    </div>
   `,
-  styles: [],
+  styles: [`
+    .container {
+      max-width: 500px;
+      margin: 0 auto;
+      padding: 20px;
+      font-family: Times, Times New Roman, serif;
+      background-color: lightgreen; 
+      border-radius: 25px; 
+      text-align: center;
+    }
+
+    .titulo {
+      color: darkblue;
+    }
+
+    .input-container {
+      margin: 20px;
+      font-weight: 600;
+    }
+
+    input {
+      padding: 5px;
+      border-radius: 10px;
+      border: 1px solid #ccc;
+    }
+  `],
 })
 export class AppComponent {
-  title = 'conversor-temperatura';
+  celsius: number = 0;
+  fahrenheit: number = 32;
+  kelvin: number = 273.15;
+
+  conversorDeCelsius() {
+    this.fahrenheit = parseFloat(((this.celsius * 9/5) + 32).toFixed(2));
+    this.kelvin = parseFloat((this.celsius + 273.15).toFixed(2));
+  }
+
+  conversorFahrenheit() {
+    this.celsius = parseFloat(((this.fahrenheit - 32) * 5/9).toFixed(2));
+    this.kelvin = parseFloat(((this.fahrenheit + 459.67) * 5/9).toFixed(2));
+  }
+
+  conversorKelvin() {
+    this.celsius = parseFloat((this.kelvin - 273.15).toFixed(2));
+    this.fahrenheit = parseFloat(((this.kelvin * 9/5) - 459.67).toFixed(2));
+  }
 }
